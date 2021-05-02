23ca42cbe9034b625fd72761fbb774d9ac0888ab
Gilberto <gilbertosa099@outlook.com>
2021-05-01 22:11:30 -0500

    Configuracion de scripts y template Home

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,32 @@
-//Manejamos las rutas en local y la nube (Es una 'herramienta de Node')
 const path = require('path');
-//Nos permite trabajar con archivos .html
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports={
-    entry:'./src/index.js',
-    output:{
+module.exports = {
+    entry: './src/index.js',
+    output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'main.js'
     },
-    resolve:{
-        extensions:['.js'],
-
+    resolve: {
+        extensions: ['.js']
     },
     module:{
-        rules:[
-            {
-                test:/\.js?$/,
-                exclude: '/node_modules',
-                use:{
-                    loader: 'babel-loader'
-                }
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
             }
-        ]
+        }]
     },
-    plugins:[
-        new HtmlWebpackPlugin([
+    plugins: [
+        new HtmlWebPackPlugin([
             {
-                inject:true,
+                inject: true,
                 template: './public/index.html',
-                fielname: './index.html'
+                filename: './index.html',
             }
         ])
     ]
+
 }
